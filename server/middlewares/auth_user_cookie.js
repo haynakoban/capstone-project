6e84a1beb6eae0af853d0ab1c5decb449ac33a90
Bryan D. Cortez <b.cortez0263@student.tsu.edu.ph>
2022-09-05 00:06:47 +0800

    generate id in cookie

--- server/middlewares/auth_user_cookie.js
+++ server/middlewares/auth_user_cookie.js
@@ -8,6 +8,9 @@ const maxAge = 1000 * 24 * 60 * 60; // 1 day
 router.use(
   session({
     key: '_uid',
+    genid: function () {
+      return require('crypto').randomBytes(64).toString('hex');
+    },
     secret: process.env.ACCESS_SECRET_KEY,
     resave: false,
     saveUninitialized: false,
