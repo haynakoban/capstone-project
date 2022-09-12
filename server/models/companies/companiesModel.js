152fcdc73de63e263f44be941fd38de9b79740a9
Bryan D. Cortez <b.cortez0263@student.tsu.edu.ph>
2022-09-12 18:44:42 +0800

    companies model

--- server/models/companies/companiesModel.js
+++ server/models/companies/companiesModel.js
@@ -5,6 +5,10 @@ const companiesSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  description: {
+    type: String,
+    required: true,
+  },
   image: {
     type: String,
     default: null,
